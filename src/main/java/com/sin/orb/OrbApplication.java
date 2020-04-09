57b542e2de00eeeb90817c0d3ae8ee8fd535eb0b
FirstSin <firstsind@gmail.com>
2020-04-09 20:12:49 +0300

    Initial commit

---
+++ src/main/java/com/sin/orb/OrbApplication.java
@@ -0,0 +1,13 @@
+package com.sin.orb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sin/orb/OrbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sin.orb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
