fcf04f744e432127bb68e7697a902b7c6183b81f
FirstSin <firstsind@gmail.com>
2020-04-10 17:32:46 +0300

    Add task displaying using ReactJS

---
+++ src/main/web/reactjs/src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Tasks from './components/tasks';
+
+class App extends Component {
+  state = {
+    tasks: []
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:8080/tasks')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ tasks: data })
+        })
+        .catch(console.log)
+  }
+  render() {
+    return (
+        <Tasks tasks={this.state.tasks} />
+    )
+  }
+}
+
+export default App;
