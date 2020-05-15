1ce8783540b64cc5f71fdf841d7167cad50e591f
FirstSin <firstsind@gmail.com>
2020-05-15 21:33:41 +0300

    Improve task cards style

--- src/main/web/reactjs/src/components/cards/TaskCard.js
+++ src/main/web/reactjs/src/components/cards/TaskCard.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import background from "../../img/done.png";
 import './TaskCard.css'
 import {Link} from "react-router-dom";
 
@@ -9,25 +6,20 @@ export default class TaskCard extends React.Component {
 
     render() {
         return (
-            <Card style={{"maxWidth": '19rem'}} className="scale">
-                <Card.Body>
-                    <Card.Title className="text-md-center font-weight-bold">{this.props.card.name}</Card.Title>
-                    <hr/>
-                    <Card.Img variant="top" src={background}/>
-                    <Card.Text className="text-muted">
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Link to={{
-                        pathname: `workspace/taskcards/${this.props.card.cardId}`,
-                        state: {
-                            card: this.props.card
-                        }
-                    }}>
-                        <Button variant="primary" block className="rounded">Open</Button>
-                    </Link>
-                </Card.Body>
-            </Card>
+            <Link to={{
+                pathname: `workspace/taskcards/${this.props.card.cardId}`,
+                state: {
+                    card: this.props.card
+                }
+            }} className="card p-card " style={{"textDecoration": "none"}}>
+                <h3 className="h3-card">{this.props.card.name}</h3>
+                <p className="small p-card">Card description with lots of great facts and interesting details.</p>
+                <div className="go-corner">
+                    <div className="go-arrow">
+                        →
+                    </div>
+                </div>
+            </Link>
         )
     }
 }
--- src/main/web/reactjs/src/components/cards/TaskCardList.js
+++ src/main/web/reactjs/src/components/cards/TaskCardList.js
@@ -1,37 +1,16 @@
 import React from 'react';
-import CardDeck from "react-bootstrap/CardDeck";
-import Row from "react-bootstrap/Row";
 import TaskCard from "./TaskCard";
+import './TaskCardList.css'
 
 export default class TaskCardList extends React.Component {
 
     render() {
-        let rowSize = 4;
-        let rowCount = 0;
-
         return (
-            <div>
-                <CardDeck>
-                    {this.splitTo(this.props.cards, rowSize).map(row => (
-                        <div className="text-md-center" key={rowCount++}>
-                            <Row style={{"margin": "15px"}}>
-                                {row.map(card => (
-                                        <TaskCard card={card} key={card.cardId}/>
-                                    )
-                                )}
-                            </Row>
-                        </div>
-                    ))}
-                </CardDeck>
+            <div className="container">
+                {this.props.cards.map(card => (
+                    <TaskCard card={card} key={card.cardId}/>
+                ))}
             </div>
         )
-    }
-
-    splitTo(arr, n) {
-        let subArray = [];
-        for (let i = 0; i < Math.ceil(arr.length / n); i++) {
-            subArray[i] = arr.slice((i * n), (i * n) + n);
-        }
-        return subArray;
     }
 }
